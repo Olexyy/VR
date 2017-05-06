22af010cec36f002b7d3985b1a48bc8505d4cac1
Olexij <inua@ua.fm>
2017-05-06 21:54:04 +0300

    Initial commit

--- modules/vr_base/js/index.js
+++ modules/vr_base/js/index.js
@@ -1,12 +1,4 @@
 function hello() {
 	alert('Hello');
-	/*var vrView = new VRView.Player('#vrview', {
-    width: '100%',
-    height: 480,
-    image: 'blank.png',
-    is_stereo: false,
-    is_autopan_off: true
-  });
-  alert(vrView);*/
 }
 hello();
